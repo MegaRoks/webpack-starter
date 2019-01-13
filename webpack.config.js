bc965e84a51df5788e7cd74b27e21a891982db7a
Maxim <MegaRoks@ya.ru>
2019-01-13 13:12:22 +0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
     entry: {
@@ -11,12 +12,52 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: '/node_modules/'
-        }]
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                      loader: 'css-loader',
+                      options: { sourceMap: true }
+                    }, 
+                    {
+                      loader: 'postcss-loader',
+                      options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }
+                    }
+                ]
+            }
+        ]
     },
     devServer: {
         overlay: true
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })
+    ]
 }
